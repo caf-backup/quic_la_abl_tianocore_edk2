af88d35439f0e04801359fd941e8c485bf1fa3a9
lijuang <lijuang@codeaurora.org>
2017-09-04 20:46:34 +0800

    QcomModulePkg: Fix the buffer overflow issue when convert string from ASCII to Unicode
    The length of the first parameter of AsciiStrToUnicodeStr maybe it's
    longer than the second one. It will cause the buffer overflow issue
    
    Change-Id: I9b5b96c1fd05f83ad0d147f852274441868a2b0d

--- QcomModulePkg/Library/FastbootLib/FastbootCmds.c
+++ QcomModulePkg/Library/FastbootLib/FastbootCmds.c
@@ -1090,6 +1090,11 @@ STATIC VOID CmdFlash(
 		FastbootFail("No data to flash");
 		return;
 	}
+
+    if (AsciiStrLen (arg) >= MAX_GPT_NAME_SIZE) {
+        FastbootFail ("Invalid partition name");
+        return;
+    }
 	AsciiStrToUnicodeStr(arg, PartitionName);
 
 	if (TargetBuildVariantUser()) {
@@ -1243,6 +1248,11 @@ STATIC VOID CmdErase(
 	CHAR16 SlotSuffix[MAX_SLOT_SUFFIX_SZ];
 	BOOLEAN MultiSlotBoot = PartitionHasMultiSlot(L"boot");
 	CHAR16 PartitionName[MAX_GPT_NAME_SIZE];
+
+    if (AsciiStrLen (arg) >= MAX_GPT_NAME_SIZE) {
+        FastbootFail ("Invalid partition name");
+        return;
+    }
 	AsciiStrToUnicodeStr(arg, PartitionName);
 
 	if (TargetBuildVariantUser()) {
@@ -1320,6 +1330,11 @@ VOID CmdSetActive(CONST CHAR8 *Arg, VOID *Data, UINT32 Size)
 	InputSlot = AsciiStrStr(Arg, Delim);
 	if (InputSlot) {
 		InputSlot++;
+        if (AsciiStrLen (InputSlot) >= MAX_SLOT_SUFFIX_SZ) {
+            FastbootFail ("Invalid Slot");
+            return;
+        }
+
 		AsciiStrToUnicodeStr(InputSlot, InputSlotInUnicode);
 		if (StrnCmp(GetCurrentSlotSuffix(), InputSlotInUnicode, StrLen(InputSlotInUnicode)))
 			SwitchSlot = TRUE;
