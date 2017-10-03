85288559a531bed870f6ebce24e418e97e069abe
lijuang <lijuang@codeaurora.org>
2017-10-03 20:06:13 +0800

    QcomModulePkg: Update the type of GetTimerCountms
    Use UINT64 instead of UINT32 to fix the potential data loss.
    
    Change-Id: I206ec4ecf0813b9eda39f6475cf01d71c5622a5a

--- QcomModulePkg/Library/BootLib/BootLinux.c
+++ QcomModulePkg/Library/BootLib/BootLinux.c
@@ -197,7 +197,8 @@ EFI_STATUS BootLinux (BootInfo *Info)
 			return EFI_BAD_BUFFER_SIZE;
 		}
 
-		DEBUG((EFI_D_INFO, "Decompressing kernel image start: %u ms\n", GetTimerCountms()));
+  DEBUG ((EFI_D_INFO, "Decompressing kernel image start: %lu ms\n",
+          GetTimerCountms ()));
 		Status = decompress(
 				(unsigned char *)(ImageBuffer + PageSize), //Read blob using BlockIo
 				KernelSize,                                 //Blob size
@@ -217,7 +218,8 @@ EFI_STATUS BootLinux (BootInfo *Info)
             return RETURN_OUT_OF_RESOURCES;
         }
 
-		DEBUG((EFI_D_INFO, "Decompressing kernel image done: %u ms\n", GetTimerCountms()));
+        DEBUG ((EFI_D_INFO, "Decompressing kernel image done: %lu ms\n",
+            GetTimerCountms ()));
 		Kptr = (struct kernel64_hdr*)KernelLoadAddr;
 	} else {
         Kptr = ImageBuffer + PageSize;
@@ -457,7 +459,8 @@ EFI_STATUS BootLinux (BootInfo *Info)
 		}
 	}
 
-	DEBUG((EFI_D_INFO, "\nShutting Down UEFI Boot Services: %u ms\n", GetTimerCountms()));
+    DEBUG ((EFI_D_INFO, "\nShutting Down UEFI Boot Services: %lu ms\n",
+        GetTimerCountms ()));
 	/*Shut down UEFI boot services*/
 	Status = ShutdownUefiBootServices ();
 	if(EFI_ERROR(Status)) {
