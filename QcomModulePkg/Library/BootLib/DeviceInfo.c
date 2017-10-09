cc688f3b7696899f79497a914bf1c70e1b38e965
lijuang <lijuang@codeaurora.org>
2017-10-09 15:57:42 +0800

    QcomModulePkg: Fix no data wipe while unlocking or locking device
    Correct the message size while writing data to device, the type of message
    is struct not const string while unlocking or locking device, it will write
    a wrong message to device.
    
    Change-Id: I5dea9c00bbb5b75fe3fa7f45b61652e26364ae5d

--- QcomModulePkg/Library/BootLib/DeviceInfo.c
+++ QcomModulePkg/Library/BootLib/DeviceInfo.c
@@ -168,8 +168,12 @@ EFI_STATUS SetDeviceUnlockValue(UINT32 Type, BOOLEAN State)
 	}
 
 	gBS->SetMem((VOID *)&Msg, sizeof(Msg), 0);
-	AsciiStrnCpyS(Msg.recovery, sizeof(Msg.recovery), RECOVERY_WIPE_DATA, AsciiStrLen(RECOVERY_WIPE_DATA));
-	WriteToPartition(&gEfiMiscPartitionGuid, &Msg);
+    Status = AsciiStrnCpyS (Msg.recovery, sizeof (Msg.recovery),
+                       RECOVERY_WIPE_DATA, AsciiStrLen (RECOVERY_WIPE_DATA));
+    if (Status == EFI_SUCCESS) {
+        Status = WriteToPartition (&gEfiMiscPartitionGuid,
+                                 &Msg, sizeof (Msg));
+    }
 
 	return Status;
 }
