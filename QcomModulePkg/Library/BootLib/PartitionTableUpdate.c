b80ea60d95144c5a094c1286289b8919da72b629
aktiwari <aktiwari@codeaurora.org>
2017-10-29 22:46:52 -0700

    Revert "QcomModulePkg : Do not decrement retry count for Android boot".
    This reverts commit 5f28962431ad79783199d54a3dfebeabcea0012b.
    
    Change-Id: I092ba9e36a36474ad3242f25d6c2e9acea9e127d

--- QcomModulePkg/Library/BootLib/PartitionTableUpdate.c
+++ QcomModulePkg/Library/BootLib/PartitionTableUpdate.c
@@ -1377,7 +1377,6 @@ EFI_STATUS FindBootableSlot(Slot *BootableSlot)
 		DEBUG((EFI_D_VERBOSE, "Active Slot %s is bootable\n",
 		       BootableSlot->Suffix));
 	} else if (Unbootable == 0 && BootSuccess == 0 && RetryCount > 0) {
-#if 0
 		if (!IsBootDevImage()) {
 			RetryCount--;
 			BootEntry->PartEntry.Attributes &= ~PART_ATT_MAX_RETRY_COUNT_VAL;
@@ -1388,7 +1387,6 @@ EFI_STATUS FindBootableSlot(Slot *BootableSlot)
 			       "Active Slot %s is bootable, retry count %ld\n",
 			       BootableSlot->Suffix, RetryCount));
 		}
-#endif
 	} else {
 		DEBUG((EFI_D_INFO,
 		       "Slot %s is unbootable, trying alternate slot\n",
