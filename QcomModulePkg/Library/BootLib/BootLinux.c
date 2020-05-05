ericcurtin/edk2

QcomModulePkg: Add support for selecting Linux Boot CPU
 XBL sets the boot cpu for boot strapping Linux. Usually
 Core 0 will be used. Now ABL can request XBL to select
 the boot CPU. With this feature we can enable the any of
 the CPU's to boot the Linux Kernel.

Change-Id: If81ffae872e7613e7adb9707eda5d033cedef102
Signed-off-by: Yadu MG <ymg@codeaurora.org>

QcomModulePkg/Library/BootLib/BootLinux.c
@@ -52,6 +52,42 @@ STATIC QCOM_SCM_MODE_SWITCH_PROTOCOL *pQcomScmModeSwitchProtocol = NULL;
 STATIC BOOLEAN BootDevImage;
 STATIC BOOLEAN IsVmComputed = FALSE;
 
+STATIC VOID
+SetLinuxBootCpu (UINT32 BootCpu)
+{
+  EFI_STATUS Status;
+  Status = gRT->SetVariable (L"DestinationCore",
+		&gQcomTokenSpaceGuid,
+		(EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_NON_VOLATILE |
+		EFI_VARIABLE_RUNTIME_ACCESS),
+		sizeof(UINT32),
+		(void*)(UINT32*)&BootCpu);
+
+  if (Status != EFI_SUCCESS) {
+       DEBUG ((EFI_D_ERROR, "Error: Failed to set Linux boot cpu:%d\n", BootCpu));
+   } else if (Status == EFI_SUCCESS) {
+       DEBUG ((EFI_D_INFO, "Switching to physical CPU:%d for Booting Linux\n", BootCpu));
+   }
+
+  return;
+}
+
+#ifdef LINUX_BOOT_CPU_SELECTION_ENABLED
+#define BootCpuId	TARGET_LINUX_BOOT_CPU_ID
+STATIC BOOLEAN
+BootCpuSelectionEnabled (VOID)
+{
+  return TRUE;
+}
+#else
+#define BootCpuId	0
+STATIC BOOLEAN
+BootCpuSelectionEnabled (VOID)
+{
+  return FALSE;
+}
+#endif
+
 /* To set load addresses, callers should make sure to initialize the
  * BootParamlistPtr before calling this function */
 UINT64 SetandGetLoadAddr (BootParamlist *BootParamlistPtr, AddrType Type)
@@ -1209,6 +1245,9 @@ skip_FfbmStr:
     if (!EFI_ERROR (Status))
       IsModeSwitch = TRUE;
   }
+
+  if(BootCpuSelectionEnabled())
+		  SetLinuxBootCpu(BootCpuId);
 
   DEBUG ((EFI_D_INFO, "\nShutting Down UEFI Boot Services: %lu ms\n",
           GetTimerCountms ()));

QcomModulePkg/QcomModulePkg.dsc
@@ -172,6 +172,14 @@
   !if $(TARGET_SUPPORTS_EARLY_USB_INIT)
       GCC:*_*_*_PP_FLAGS = -DTARGET_SUPPORTS_EARLY_USB_INIT
   !endif
+  !if $(LINUX_BOOT_CPU_SELECTION_ENABLED)
+      GCC:*_*_*_CC_FLAGS = -DLINUX_BOOT_CPU_SELECTION_ENABLED
+      GCC:*_*_*_CC_FLAGS = -DTARGET_LINUX_BOOT_CPU_ID=$(TARGET_LINUX_BOOT_CPU_ID)
+  !endif
+  !if $(LINUX_BOOT_CPU_SELECTION_ENABLED)
+      GCC:*_*_*_PP_FLAGS = -DLINUX_BOOT_CPU_SELECTION_ENABLED
+      GCC:*_*_*_PP_FLAGS = -DTARGET_LINUX_BOOT_CPU_ID=$(TARGET_LINUX_BOOT_CPU_ID)
+  !endif
 
 [PcdsFixedAtBuild.common]
 

caf-backup/quic_la_abl_tianocore_edk2

QcomModulePkg/Library/BootLib/BootLinux.c
@@ -53,6 +53,42 @@ STATIC QCOM_SCM_MODE_SWITCH_PROTOCOL *pQcomScmModeSwitchProtocol = NULL;
 STATIC BOOLEAN BootDevImage;
 STATIC BOOLEAN IsVmComputed = FALSE;
 
+STATIC VOID
+SetLinuxBootCpu (UINT32 BootCpu)
+{
+  EFI_STATUS Status;
+  Status = gRT->SetVariable (L"DestinationCore",
+		&gQcomTokenSpaceGuid,
+		(EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_NON_VOLATILE |
+		EFI_VARIABLE_RUNTIME_ACCESS),
+		sizeof(UINT32),
+		(void*)(UINT32*)&BootCpu);
+
+  if (Status != EFI_SUCCESS) {
+       DEBUG ((EFI_D_ERROR, "Error: Failed to set Linux boot cpu:%d\n", BootCpu));
+   } else if (Status == EFI_SUCCESS) {
+       DEBUG ((EFI_D_INFO, "Switching to physical CPU:%d for Booting Linux\n", BootCpu));
+   }
+
+  return;
+}
+
+#ifdef LINUX_BOOT_CPU_SELECTION_ENABLED
+#define BootCpuId	LINUX_BOOT_CPU_ID
+STATIC BOOLEAN
+BootCpuSelectionEnabled (VOID)
+{
+  return TRUE;
+}
+#else
+#define BootCpuId	0
+STATIC BOOLEAN
+BootCpuSelectionEnabled (VOID)
+{
+  return FALSE;
+}
+#endif
+
 /* To set load addresses, callers should make sure to initialize the
  * BootParamlistPtr before calling this function */
 UINT64 SetandGetLoadAddr (BootParamlist *BootParamlistPtr, AddrType Type)
@@ -1138,6 +1174,9 @@ BootLinux (BootInfo *Info)
     if (!EFI_ERROR (Status))
       IsModeSwitch = TRUE;
   }
+
+  if(BootCpuSelectionEnabled())
+		  SetLinuxBootCpu(BootCpuId);
 
   DEBUG ((EFI_D_INFO, "\nShutting Down UEFI Boot Services: %lu ms\n",
           GetTimerCountms ()));

QcomModulePkg/QcomModulePkg.dsc
@@ -153,6 +153,14 @@
   !if $(TARGET_SUPPORTS_EARLY_INIT)
       GCC:*_*_*_CC_FLAGS = -DENABLE_EARLY_SERVICES
   !endif
+  !if $(LINUX_BOOT_CPU_SELECTION_ENABLED)
+      GCC:*_*_*_CC_FLAGS = -DLINUX_BOOT_CPU_SELECTION_ENABLED
+      GCC:*_*_*_CC_FLAGS = -DLINUX_BOOT_CPU_ID=$(TARGET_LINUX_BOOT_CPU_ID)
+  !endif
+  !if $(LINUX_BOOT_CPU_SELECTION_ENABLED)
+      GCC:*_*_*_PP_FLAGS = -DLINUX_BOOT_CPU_SELECTION_ENABLED
+      GCC:*_*_*_PP_FLAGS = -DLINUX_BOOT_CPU_ID=$(TARGET_LINUX_BOOT_CPU_ID)
+  !endif
 
 [PcdsFixedAtBuild.common]
 