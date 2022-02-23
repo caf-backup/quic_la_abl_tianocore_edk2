08a464cb3a88ced8251c953d976be59daadfb89d
Venkata Rao Kakani <quic_vkakani@quicinc.com>
2022-02-23 12:18:24 +0530

    Verifiedboot: Change LF Copyrights
    Correct LF Copyrights with proper markings.
    
    Change-Id: I1a2135d1b86c30b67aae32667cd2b166cca3d649
    Signed-off-by: Venkata Rao Kakani <quic_vkakani@quicinc.com>

--- QcomModulePkg/Library/FastbootLib/FastbootCmds.c
+++ QcomModulePkg/Library/FastbootLib/FastbootCmds.c
@@ -18,7 +18,8 @@ found at
  * Copyright (c) 2009, Google Inc.
  * All rights reserved.
  *
- * Copyright (c) 2015 - 2022, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015 - 2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -44,6 +45,7 @@ found at
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
 */
 #include <Library/BaseLib.h>
 #include <Library/BaseMemoryLib.h>
